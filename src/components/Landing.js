f48102afd62653d15717802cac5e545ed02850bf
Taylor Cool <taycool@gmail.com>
2019-11-12 16:57:37 -0800

    updated styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,13 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Landing = () => {
     return(
-        <div className='hero-img'>
-            <img src='src/images/codec.jpg' alt='Metal Gear Solid Codec' />
-            <div className='hero-text'>
-                <h1>The Official Unofficial Kojima Name Generator</h1>
-                <h3>Generate Your Kojima-Name Now!</h3>
-                {/* <Link>Get Started</Link> */}
-            </div>
+        <div className='header'>
+            <h1>The Official Unofficial Kojima Name Generator</h1>
+            <h3>Generate Your Kojima-Name Now!</h3>
+            <Link>Get Started</Link>
         </div>
 )}
 
