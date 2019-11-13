676ac75e0325b2904ae893671101293d7d569c44
Taylor Cool <taycool@gmail.com>
2019-11-12 19:01:25 -0800

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Landing from './components/Landing';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
@@ -7,12 +8,18 @@ import Section3 from './components/Section3';
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-      {/* <Section1 />
-      <Section2 />
-      <Section3 /> */}
-    </div>
+    <Router >
+      <div className="App">
+        <Landing />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        {/* <Route exact path='/' component={Landing} />
+        <Route path='/section/1' component={Section1}/>
+        <Route path='/section/2' component={Section1}/>
+        <Route path='/section/3' component={Section1}/> */}
+      </div>
+    </Router>
   );
 }
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Landing = () => {
     return(
         <div className='header'>
+            <img url='./images/codec.jpg' />
             <h1>The Official Unofficial Kojima Name Generator</h1>
             <h3>Generate Your Kojima-Name Now!</h3>
-            <Link>Get Started</Link>
+            <Link className='startButton' to='/section/1'>Get Started</Link>
         </div>
 )}
 
